76e856048074fba7ff25b3c515d666b5892cd276
Tinjis <thecybertrojan@gmail.com>
2025-01-19 20:23:23 +0100

    Add files via upload

---
+++ chapter 1/Exercises/Exercise 4/Exercise_4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int x;
+   int y;
+   int z;
+
+   float a;
+   float b;
+   float c;
+
+   printf("%d\n", x);
+   printf("%d\n", y);
+   printf("%d\n", z);
+
+   printf("%f\n", a);
+   printf("%f\n", b);
+   printf("%f\n", c);
+
+}
---
+++ chapter 1/Exercises/Exercise 7/Exercise_7.md
@@ -0,0 +1,7 @@
+Keywords:
+(a) for
+(e) while
+Not Keywords:
+(b) If (should be if)
+(c) main
+(d) printf
